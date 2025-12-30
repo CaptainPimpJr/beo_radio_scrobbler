2d50ea75fdd6407d418d78077093697f152d55d8
Lucas <prio3@icloud.com>
2025-12-30 09:30:29 +0100

    new appdata folder

--- src/beo_radio_scrobbler/config.py
+++ src/beo_radio_scrobbler/config.py
@@ -13,7 +13,10 @@ else:
 beo_ip = os.getenv("BEO_IP", default='')
 local_time = os.getenv("LOCAL_TIMEZONE", default='UTC')  #e.g. "Europe/Berlin"
 run_mode = os.getenv("RUN_MODE", default='detect_smpl')  #development or production
-station_rules_file = Path(Path.cwd() / "data" / "config" / "station_rules.yaml")
+try:
+    station_rules_file = Path(Path.cwd() / "appdata" / "config" / "station_rules.yaml")
+except:
+    os._exit()
 
 # logging
 logger.level("SCROBBLE", no=25, color="<yellow>", icon="🎵")
@@ -21,28 +24,28 @@ logger.level("STATION", no=26, color="<green>", icon="📻")
 logger.level("NOTIFICATION", no=27, color="<blue>", icon="🔔")
 
 logger.add(
-    Path(Path.cwd() / "data" / "logs" / "log_radio_scrobbler.log"),
+    Path(Path.cwd() / "appdata" / "logs" / "log_radio_scrobbler.log"),
     rotation="00:00",
     retention="7 days",
     format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
 )
 if run_mode == 'production':
     logger.add(
-        Path(Path.cwd() / "data" / "logs" / "log_scrobbles.log"),
+        Path(Path.cwd() / "appdata" / "logs" / "log_scrobbles.log"),
         rotation="w1",
         retention="5 years",
         format="{time:YYYY-MM-DD HH:mm:ss} | {message}",
         filter=lambda record: record["level"].no == 25)
 elif run_mode in ['detect', 'detect_smpl']:
     logger.add(
-        Path(Path.cwd() / "data" / "logs" / "log_detections.log"),
+        Path(Path.cwd() / "appdata" / "logs" / "log_detections.log"),
         rotation="00:00",
         retention="1 week",
         format="{time:YYYY-MM-DD HH:mm:ss} | {message}",
         filter=lambda record: record["level"].no == 26)
 elif run_mode == 'notify_me':
     logger.add(
-        Path(Path.cwd() / "data" / "logs" / "log_notifications.log"),
+        Path(Path.cwd() / "appdata" / "logs" / "log_notifications.log"),
         rotation="00:00",
         retention="7 days",
         format="{time:YYYY-MM-DD HH:mm:ss} | {message}",
---
+++ src/beo_radio_scrobbler/utils/init.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import shutil
+from ..config import logger
+
+async def initialize_logging():
+    if Path(Path.cwd() / "appdata" / "logs").exists():
+        pass
+    else:
+        Path(Path.cwd() / "appdata" / "logs").mkdir(parents=True, exist_ok=True)
+        logger.info("Created logs directory.")
+
+    sample_file = Path(Path.cwd() / "appdata" / "logs" / "sample-log_radio_scrobbler.log")
+    if sample_file.exists():
+        pass
+    else:
+        shutil.copy(Path(Path.cwd() / "data" / "logs" / "sample-log_radio_scrobbler.log"), sample_file)
+        logger.info("Created sample log file.")
+       
+
+async def initialize_config():
+    if Path(Path.cwd() / "appdata" / "config").exists():
+        pass
+    else:
+        Path(Path.cwd() / "appdata" / "config").mkdir(parents=True, exist_ok=True)
+        logger.info("Created config directory.")
+
+    sample_file = Path(Path.cwd() / "appdata" / "config" / "sample-station_rules.yaml")
+    if sample_file.exists():
+        pass
+    else:
+        shutil.copy(Path(Path.cwd() / "data" / "config" / "sample-station_rules.yaml"), sample_file)
+        logger.info("Created sample station rules file.")
